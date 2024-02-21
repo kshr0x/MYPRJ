73efb686352d1f14999f4c122db2af1da1805c79
Kirill Borzov <kirillborzov1405@gmail.com>
2024-02-21 17:23:14 +0300

    New function in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
+int summ(int a, int b);
 int main()
 {
+int a=5;
+int b=6;
 cout << "Hello world" << endl;
+cout << summ(a, b);
 return 0;
+}
+int summ(int a, int b);
+{
+return a+b;
 }
